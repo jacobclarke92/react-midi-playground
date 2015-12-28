0101da63d1685abfbd9d6d70f607efb31929a1b6
Jacob Clarke <jacobclarke92@gmail.com>
2015-12-28 22:57:26 +1000

    Added data status indicators

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import _ from 'lodash'
 
 import * as Midi from 'api/Midi'
 
+
 export default class App extends Component {
 
 	constructor(props) {
 		super(props);
 		this.ctrlKeyPressed = false;
+		this.deviceDataTimeouts = [];
+		this.lastMidiMessage = null;
+
 		this.updateDevices = _.debounce(this.updateDevices, 250);
 		this.state = {
 			devices: [],
 			activeDevices: [],
+			devicesReceivingData: [],
 		};
 	}
 
@@ -34,7 +39,6 @@ export default class App extends Component {
 	@autobind
 	handleKeyDown(e) {
 		switch(keyCode(e)) {
-			case 'ctrl': this.ctrlKeyPressed = true; break;
 			case 'command': this.ctrlKeyPressed = true; break;
 		}
 	}
@@ -42,7 +46,6 @@ export default class App extends Component {
 	@autobind
 	handleKeyUp(e) {
 		switch(keyCode(e)) {
-			case 'ctrl': this.ctrlKeyPressed = false; break;
 			case 'command': this.ctrlKeyPressed = false; break;
 		}
 	}
@@ -53,11 +56,30 @@ export default class App extends Component {
 	}
 
 	@autobind
+	handleMidiMessage(device, message) {
+		let { devicesReceivingData } = this.state;
+		this.lastMidiMessage = message;
+		if(!_.contains(devicesReceivingData, device)) {
+			devicesReceivingData = [...devicesReceivingData, device];
+			this.setState({devicesReceivingData});
+		}
+		if(this.deviceDataTimeouts[device.id]) clearTimeout(this.deviceDataTimeouts[device.id]);
+		this.deviceDataTimeouts[device.id] = setTimeout(() => {
+			devicesReceivingData = devicesReceivingData.filter(_device => !_.isEqual(_device, device));
+			this.setState({devicesReceivingData})
+		}, 50);
+	}
+
+	@autobind
 	updateDevices() {
 		const devices = Midi.getMidiInputDevices();
 		const deviceNames = devices.map(device => device.name);
 		console.log('Current input devices: ', deviceNames);
 		this.setState({devices});
+		
+		for(let device of devices) {
+			device.onmidimessage = event => this.handleMidiMessage(device, event.data);
+		}
 	}
 
 	@autobind
@@ -74,16 +96,27 @@ export default class App extends Component {
 	}
 
 	render() {
-		const { devices, activeDevices } = this.state;
+		const { devices, activeDevices, devicesReceivingData } = this.state;
 		return (
 			<div>
 				<h1>React MIDI Interface</h1>
-				<p>Current MIDI input devices:</p>
-				<ul className="devices">
-					{devices.map((device,i) =>
-						<li key={i} className={_.contains(activeDevices, device) && 'active'} onClick={event => this.handleDeviceClick(device)}>{device.name}</li>
-					)}
-				</ul>
+				<div className="status">
+
+				</div>
+				<div className="devices">
+					<p>Current MIDI input devices:</p>
+					<ul>
+						{devices.map((device,i) =>
+							<li key={i} className={_.contains(activeDevices, device) && 'active'} onClick={event => this.handleDeviceClick(device)}>
+								{(_.contains(devicesReceivingData, device) ? '◉ ' : '◎ ') + device.name}
+							</li>
+						)}
+					</ul>
+					<br />
+					<ul>
+						<li>{this.lastMidiMessage && this.lastMidiMessage.join(', ')}</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
